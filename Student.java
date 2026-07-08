b231c80273cb8154d0cec8a6c490632c0a873caa
Ju1iusA <juliusaguilar03@gmail.com>
2026-07-08 11:21:16 -0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,15 @@
+package iVoteSim;
+
+public class Student {
+    private String id;
+
+    //Creates student
+    public Student(String inputId) {
+        id = inputId;
+    }
+
+    //Returns student id
+    public String getId() {
+        return id;
+    }
+}
